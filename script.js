7ee9a0ee926a851191dd12c3449ccad4215eaaed
nrbrt2002 <nrbrt2002@gmail.com>
2023-07-18 10:01:14 +0200

    after adding dynamic griding prompt and it works perfect

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const sizeOfGrids = 16;
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('button');
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
     for (let i = 0; i< amtOfGrids; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
 
         for(let j = 0; j< amtOfGrids; j++){
-            const WidthAndHeight = 460 / sizeOfGrids;
+            const WidthAndHeight = 460 / amtOfGridsgit;
             const gridBox = document.createElement('div');
             gridBox.classList.add('grid-box');
             gridBox.style.width = `${WidthAndHeight}px`;
@@ -17,11 +20,30 @@ const createGrid = (amtOfGrids) => {
             });
             row.appendChild(gridBox);       
         }
-        container.appendChild(row);
+        wrapper.appendChild(row);
     }
+    container.appendChild(wrapper);
 }
+// createGrid(sizeOfGrids);     
 
-createGrid(sizeOfGrids);
+resetButton.addEventListener('click', ()=>{
+    let userSize =Number(prompt('Enter Dimensions'));
+
+    while(userSize > 100){
+        userSize =Number(prompt('Dimensions can\'t Exicced 100 Grids. Try Again'));
+    }
+    const wrapper = document.querySelector('.wrapper');
+
+    if(!wrapper){
+        createGrid(userSize);
+    }else{
+        wrapper.remove();
+        createGrid(userSize);
+    }
+     
+});
+
+// createGrid(sizeOfGrids);
 
 // const allDivs = document.querySelectorAll('.grid-box');
 // allDivs.forEach(div => {
